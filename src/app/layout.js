8a923f90ad3eb2db0ac06950e0c861e9aa878c6a
Renuka Singh <renuka@foliumlabs.net>
2025-06-22 20:15:16 +0530

    added google analytics code

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ECTL0ZQ1BE');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
