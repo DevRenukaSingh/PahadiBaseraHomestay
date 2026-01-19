abf05144ed4bdb6e05ac139bbc385da5968f4872
DevRenukaSingh <renukasingh0710@gmail.com>
2026-01-19 21:57:02 +0530

    removed navbar from hero file, modified theme btn

--- components/hero.js
+++ components/hero.js
@@ -2,24 +2,28 @@
 
 import Image from 'next/image'
 import Button from '../widgets/button';
-import Navbar from './navbar';
 
 export default function Hero(){
 
     return(<div className="w-full hero-bg">
-        <Navbar />
         <Image
             src="https://res.cloudinary.com/db4clvkdl/image/upload/v1756136143/summers-in-mountainhome_qlzdob.webp"
             alt="summers in mountain home"
             width={4032}
             height={3024}
             quality={[50,75]}
+            style={imageStyle}
         />
-        {/* <div className='overlay'/>  */}
         <div className='overlay-content text-center'>
-            <h1 className='text-white text-2xl pb-4 hero-heading'>PAHADI BASERA HOMESTAY</h1>
+            <h1 className='text-white text-3xl pb-4 hero-heading'>PAHADI BASERA HOMESTAY</h1>
             <p className='text-white text-sm pb-4 px-8 '>A peaceful mountain escape near Manali with orchard views and a warm Kathkuni welcome.</p>
-            <Button text='Book your stay' onClick={()=>{ window.open('https://wa.me/+919910859297?text=Hello,%20can%20I%20get%20more%20info%20about%20your%20homestay?','_blank')}}/>
+            <Button text='Check Availability' onClick={()=>{ window.open('https://wa.me/+919910859297?text=Hello,%20can%20I%20get%20more%20info%20about%20your%20homestay?','_blank')}}/>
         </div>
     </div>)
+}
+
+
+const imageStyle = {
+  width: '100vw',
+  height: '100vh',
 }
--- widgets/button.js
+++ widgets/button.js
@@ -1,14 +1,9 @@
 
-
-
-export default function Button({text='ok',onClick={},size=1,type='default'}){
+export default function Button({text='ok',onClick={} ,type='default'}){
 
  return(
     <div>
-    <button 
-    onClick={onClick}
-    className="theme-btn font-medium rounded-full 
-    text-sm px-5 py-2.5 text-center me-2 mb-2 ">{text}
+    <button onClick={onClick} className="theme-btn font-medium rounded-full text-md text-center me-2 mb-2">{text}
     </button>
     </div>
     )
